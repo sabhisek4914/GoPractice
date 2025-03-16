94e3600b80827bb60b9a88b32605235d32518452
sabhisek4914 <subham.abhisek4914@gmail.com>
2025-03-16 22:08:21 +0530

    16 March 25

---
+++ DS/4. Sorting/1. MergeSort/main.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+/*
+MergeSort ->Divide and conquer
+O(nlogn)
+Eg:
+Input:	[10,5,2,0,7,6,4]
+Output: [0,2,,4,5,6,7,10]
+*/
+
+func main() {
+	arr := []int{10, 5, 2, 0, 7, 6, 4}
+	mergeSort(arr, 0, len(arr)-1)
+	fmt.Println(arr)
+}
+
+func mergeSort(arr []int, s, e int) {
+	if s >= e {
+		return
+	}
+	m := (s + e) / 2
+	mergeSort(arr, s, m)
+	mergeSort(arr, m+1, e)
+	merge(arr, s, e)
+}
+
+func merge(arr []int, s, e int) {
+	m := (s + e) / 2
+	i := s
+	j := m + 1
+	temp := []int{}
+	// k := 0
+	for i <= m && j <= e {
+		if arr[i] <= arr[j] {
+			temp = append(temp, arr[i])
+			i++
+
+		} else {
+			temp = append(temp, arr[j])
+			j++
+
+		}
+	}
+	for j <= e {
+		temp = append(temp, arr[j])
+		j++
+	}
+	for i <= m {
+		temp = append(temp, arr[i])
+		i++
+	}
+	arr = temp
+	// Copy sorted elements back to original array
+	// copy(arr[s:e+1], temp)
+}
